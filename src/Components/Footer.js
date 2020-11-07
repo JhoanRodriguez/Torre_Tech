ea44b0e9784ebb734cfbcd51a63ac38de883fe02
Jhoan Rodriguez <jhoansebastian@msn.com>
2020-11-07 12:24:40 -0500

    Config the Header,Footer and Main component

---
+++ src/Components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div className="footer" >
+                <div className="container">
+                    <p><strong>Jhoan Rodríguez Ⓒ All Rights reserved</strong></p>
+                    <br></br>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Footer
---
+++ src/Components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="header" >
+                <div className="container">
+                    <h1 className="display-5">Torre Resumé</h1>
+                    <br></br>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Header
